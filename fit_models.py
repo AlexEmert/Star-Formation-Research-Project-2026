21495457886bde987957ac18a307fa2f4b5f3c8c
EricFriedlander <efriedlander@collegeofidaho.edu>
2026-05-22 13:03:10 -0600

    Added stuff

---
+++ fit_models.py
@@ -0,0 +1,5 @@
+# In this file we run some pythong code that takes advantage of njobs=-1.
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
---
+++ submit_fit_models.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+#SBATCH -c 48
+#SBATCH -t 12:00
+#SBATCH -N 1
+
+mamba activate starform-alex
+python fit_models.pyls

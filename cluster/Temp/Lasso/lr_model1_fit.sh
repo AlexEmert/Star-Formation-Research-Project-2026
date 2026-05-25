20d5a370c5ebc5af73a1414b9cab520d8565d14e
AlexEmert <alexemert@gmail.com>
2026-05-25 15:21:43 -0600

    correct sbatch code

--- cluster/Temp/Lasso/lr_model1_fit.sh
+++ cluster/Temp/Lasso/lr_model1_fit.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 #SBATCH -c 48
 #SBATCH -N 1
+#SBATCH -J linear_model_fit
+#SBATCH -p bsudfq
+#SBATCH -t 00:10:00
 
+
+. ~/.bashrc
 mamba activate starform-alex
 python lrmodel1.py
-mamba deactivate starform-alex

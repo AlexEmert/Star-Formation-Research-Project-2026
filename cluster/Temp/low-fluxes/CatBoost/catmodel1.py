e3e5d30cd22010785bba6d8ce0a6bbcc00261e0f
AlexEmert <alexemert@gmail.com>
2026-05-26 10:57:23 -0600

    catmodel update packages

--- cluster/Temp/low-fluxes/CatBoost/catmodel1.py
+++ cluster/Temp/low-fluxes/CatBoost/catmodel1.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split, GridSearchCV
-from cluster.ratio_function import RatioGenerator
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler

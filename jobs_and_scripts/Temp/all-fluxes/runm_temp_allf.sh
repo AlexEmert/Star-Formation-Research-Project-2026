f4d31c64648086db70dd8f18f0e438091d590537
AlexEmert <alexemert@gmail.com>
2026-06-01 16:56:15 -0600

    all flux temp model setup

---
+++ jobs_and_scripts/Temp/all-fluxes/runm_temp_allf.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=48
+#SBATCH -J temp_model_fit
+#SBATCH -p bsudfq
+#SBATCH -t 03:00:00
+#SBATCH --output=out_temp_allf.out
+
+
+. ~/.bashrc
+mamba activate starform-alex
+python mod_temp_allf.py

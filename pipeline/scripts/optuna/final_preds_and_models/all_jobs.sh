5d7d775b911cfb844fadd7c1ef78c653f5be0eda
AlexEmert <alexemert@gmail.com>
2026-07-20 10:34:03 -0600

    final predictive modeling scripts

---
+++ pipeline/scripts/optuna/final_preds_and_models/all_jobs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+RESPONSES=("LRATIO" "LM" "L_BOL" "MASS" "DIAM" "SURF_DENS" "TEMP" "T_BOL")
+
+# just in case something goes wrong, automatically makes results and logs folders
+mkdir -p ../logs ../results
+
+
+for r in "${RESPONSES[@]}"; do
+
+   sbatch --export=ALL,RESPONSE="$r",ITERS="320" run_job.sh
+
+   sleep 0.1
+done
---
+++ pipeline/scripts/optuna/final_preds_and_models/run_job.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -J model_fit      # job name
+#SBATCH -n 1              # total number of tasks requested
+#SBATCH -c 48             # CPU cores per task
+#SBATCH -N 1              # number of nodes you want to run on
+#SBATCH -p bsudfq         # queue (partition)
+#SBATCH -t 2-00:00:00     # run time (hh:mm:ss)
+#SBATCH --output=../logs/%x_%j.out # output and error file name (%j expands to jobID)
+
+
+. ~/.bashrc
+mamba activate starform-alex
+
+python optuna_search_mult_quant_reg.py \
+    --iters "$ITERS" \
+    --response "$RESPONSE"
